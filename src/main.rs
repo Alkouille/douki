606f32f841745b367d0f283f7b28a4d3f2c4ba8a
Alkouille <woer.ilian@gmail.com>
2021-04-05 18:43:14 +0200

    renamed api to ani_api, MyQuery renamed to ListQuery, ListQuery now take user by name (used to be by id)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-mod api;
-use crate::api::queries::MyQueryArguments;
+mod ani_api;
+use crate::ani_api::queries::{ListQueryArguments, MediaType::Manga};
 
 use cynic;
 use cynic::{http::SurfExt, QueryBuilder};
+use std::io;
 use tokio;
 
 #[tokio::main]
+
+/* example */
 async fn main() {
-    let query = api::queries::MyQuery::build(MyQueryArguments { id: 701603 });
+    let mut ani_username = String::new();
+
+    println!("Please specifiy your anilist username");
+    io::stdin()
+        .read_line(&mut ani_username)
+        .expect("Failed to read line");
+
+    let query = ani_api::queries::ListQuery::build(ListQueryArguments {
+        name: ani_username,
+        list_type: Manga,
+    });
 
     let response = surf::post("https://graphql.anilist.co/")
         .run_graphql(query)
