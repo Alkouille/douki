55d07e8974b8e55dc833830e28f762816dd83c0d
Alkouille <woer.ilian@gmail.com>
2021-04-05 18:11:03 +0200

    renamed API.rs to api.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod API;
-use crate::API::queries::MyQueryArguments;
+mod api;
+use crate::api::queries::MyQueryArguments;
 
 use cynic;
 use cynic::{http::SurfExt, QueryBuilder};
@@ -7,7 +7,7 @@ use tokio;
 
 #[tokio::main]
 async fn main() {
-    let query = API::queries::MyQuery::build(MyQueryArguments { id: 701603 });
+    let query = api::queries::MyQuery::build(MyQueryArguments { id: 701603 });
 
     let response = surf::post("https://graphql.anilist.co/")
         .run_graphql(query)
